9878739730d044beb57b894c2e78fc9b11ed9f17
Quentin Read <quentin.read@gmail.com>
2021-04-14 01:12:11 -0400

    whoops add runtime:shiny

--- pkgsetup.R
+++ pkgsetup.R
@@ -4,3 +4,9 @@ use_vignette(name = 'testshiny', title = 'test embedded Shiny app')
 use_package('shiny')
 use_github()
 use_pkgdown_github_pages()
+
+webshot::install_phantomjs()
+
+pkgdown::build_site()
+
+devtools::install_github('qdread/testShinyVignette', build_vignettes = TRUE)
